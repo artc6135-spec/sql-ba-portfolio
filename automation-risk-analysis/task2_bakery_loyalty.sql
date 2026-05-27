f6e255cc56ba227875a156ce2e902020f239f330
artc6135-spec <artc6135@gmail.com>
2026-05-27 12:35:09 +0200

    Directory alignment

--- automation-risk-analysis/task2_bakery_loyalty.sql
+++
@@ -1,7 +0,0 @@
-SELECT u.city, COUNT(o.order_id) AS delivered_orders_count
-FROM users u 
-JOIN orders o ON o.user_id = u.user_id
-WHERE u.loyalty_level IN ('Silver', 'Gold', 'Platinum')
-  AND o.delivery_status = 'Delivered'
-GROUP BY u.city
-HAVING COUNT(o.order_id) > 2;
